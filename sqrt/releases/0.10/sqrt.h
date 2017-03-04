6b3449e95c1f51b513967827d814c69f069096ca
yu-yama <yudai.yamazaki@gmail.com>
2017-03-04 19:59:12 +0900

    Create sqrt.h

---
+++ sqrt/releases/0.10/sqrt.h
@@ -0,0 +1,12 @@
+#ifndef _SQRT_H_
+#define _SQRT_H_
+
+class CSqrt{
+public:
+    int divide_counter(int a);
+    void divide(int a, int digit, int *divided);
+    void reverse_array(int *original, int digit);
+    void sqrt(int *factorial,int digit, int digit2, int *ans);
+};
+
+#endif //_SQRT_H_
